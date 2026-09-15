99abe0c088805fbcec3e2ed85a1bcbad9d7e4b41
Safra Skin <safra.skin@local.dev>
2026-09-15 20:08:25 +0200

    Use provided TikTok base snippet in head; remove duplicate deferred loader

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -49,10 +49,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     `
     : "";
 
-  const tiktokScript = tiktokPixelId
-    ? `
-      !function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=['page','track','identify','instances','debug','on','off','once','ready','alias','group','enableCookie','disableCookie'];ttq.setAndDequeue=function(t,e){t=Array.prototype.slice.call(t);ttq.push(['set',e||{}]);if(!this._d)this._d=t};ttq._i=Date.now();ttq._d=ttq._d||[];(function(){var tt=d.createElement('script');tt.type='text/javascript';tt.async=true;tt.src='https://analytics.tiktok.com/i18n/pixel/sdk.js';var s=d.getElementsByTagName('script')[0];s.parentNode.insertBefore(tt,s)})();}(window,document,'ttq');ttq.load('${tiktokPixelId}');ttq.page();
-    `
+  const tiktokBaseSnippet = tiktokPixelId
+    ? `<!-- TikTok Pixel Code Start -->\n<script>\n!function (w, d, t) {\n  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(\nvar e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")\n;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};\n\n\n  ttq.load('${tiktokPixelId}');\n  ttq.page();\n}(window, document, 'ttq');\n</script>\n<!-- TikTok Pixel Code End -->`
     : "";
 
   return (
@@ -66,7 +64,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         />
         <link rel="preload" as="image" href="/brand/logo.webp" />
         {metaScript ? <script dangerouslySetInnerHTML={{ __html: metaScript }} /> : null}
-        {tiktokScript ? <script dangerouslySetInnerHTML={{ __html: tiktokScript }} /> : null}
+        {tiktokBaseSnippet ? <script dangerouslySetInnerHTML={{ __html: tiktokBaseSnippet }} /> : null}
       </head>
       <body className={`${arabic.variable} ${english.variable}`}>
         <StoreShell>{children}</StoreShell>
